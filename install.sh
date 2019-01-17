7514c7b30f12943a1cd5d45e4edea284d79cdff1
Danny <danny.meyer@gmail.com>
2019-01-17 15:59:35 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+sudo apt-get install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
+curl 'https://gist.githubusercontent.com/xfanwu/18fd7c24360c68bab884/raw/f09340ac2b0ca790b6059695de0873da8ca0c5e5/xxf.zsh-theme' > ~/.oh-my-zsh/themes/xxf.zsh-theme
+sed -i 's/ZSH_THEME=.*/ZSH_THEME="xxf"/' ~/.zshrc
+
+wget https://raw.githubusercontent.com/skaji/remote-pbcopy-iterm2/master/pbcopy
+chmod +x pbcopy
+mv pbcopy /usr/local/bin/
+
+ln -sf ~/batteries/.zshrc ~/.zshrc
