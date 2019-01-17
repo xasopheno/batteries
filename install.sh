e39001114306bc65061a76b8691da9f7edfaeb68
Danny Meyer <danny.meyer@gmail.com>
2019-01-17 21:30:11 +0000

    wip

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 curl 'https://gist.githubusercontent.com/xfanwu/18fd7c24360c68bab884/raw/f09340ac2b0ca790b6059695de0873da8ca0c5e5/xxf.zsh-theme' > ~/.oh-my-zsh/themes/xxf.zsh-theme
 sed -i 's/ZSH_THEME=.*/ZSH_THEME="xxf"/' ~/.zshrc
 
-wget https://raw.githubusercontent.com/skaji/remote-pbcopy-iterm2/master/pbcopy
+curl https://raw.githubusercontent.com/skaji/remote-pbcopy-iterm2/master/pbcopy > pbcopy
 chmod +x pbcopy
 sudo mv pbcopy /usr/local/bin/
 
@@ -14,3 +14,5 @@ ln -sf ~/batteries/.zshrc ~/.zshrc
 
 git config --global user.email "danny.meyer@gmail.com"
 git config --global user.name "Danny Meyer"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
